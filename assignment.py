a57c36d0e817b090c15630eeee31190f9ea57dfd
gopa354 <42486206+gopa354@users.noreply.github.com>
2018-08-20 23:59:41 +0530

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,11 @@
+try:
+    input_data=eval(input('Enter u r data:-'))
+    if type(input_data) == int:
+        print(input_data,'Entered input is int type only')
+    elif type(input_data) == str:
+        if input_data.isdigit():
+            print(input_data,'Entered input is int in string')
+        else:
+            print(input_data,'Entered input is sting only')
+except NameError:
+    print('its string')
